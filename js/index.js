9ca3ea566d00b0407360027f86000e1b80d25cf4
Victor Henrique Souza <vhs.desenvolvedorweb@gmail.com>
2026-01-04 23:45:58 -0300

    update 1

---
+++ js/index.js
@@ -0,0 +1,5 @@
+function openMenu() {
+    console.log('O menu foi aberto.');
+    document.querySelector('header').classList.toggle('open');
+    document.querySelector('.items').classList.toggle('open');
+};
